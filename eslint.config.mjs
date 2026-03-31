8927402fec6545d86cef3190ba229735c3764bd7
Paul Kaggwa <thepkay@gmail.com>
2026-03-31 14:09:47 +0300

    refactor: migrated react front-end to use typescript

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,40 +1,46 @@
+import js from "@eslint/js"
+import globals from "globals"
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from "eslint/config";
-import globals from "globals";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { FlatCompat } from '@eslint/eslintrc'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 const compat = new FlatCompat({
     baseDirectory: __dirname,
     recommendedConfig: js.configs.recommended,
     allConfig: js.configs.all
-});
+})
 
-export default defineConfig([globalIgnores(["**/dist/", "**/vite.config.js"]), {
-    extends: compat.extends(
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:jsx-a11y/recommended",
-        "prettier",
-    ),
-
-    languageOptions: {
-        globals: {
-            ...globals.browser,
+export default defineConfig([
+    globalIgnores(['**/dist/', '**/vite.config.ts']),
+    {
+        files: ['**/*.{ts,tsx}'],
+        extends: [
+            js.configs.recommended,
+            tseslint.config.recommended,
+            reactHooks.configs.flat.recommended,
+            reactRefresh.configs.vite,
+            compat.extends(
+                /*'eslint:recommended',*/
+                /*'plugin:react/recommended',*/
+                'plugin:jsx-a11y/recommended',
+                'prettier',
+            ),
+        ],
+        languageOptions: {
+            ecmaVersion: 2020,
+            sourceType: 'module',
+            globals: globals.browser,
         },
-
-        ecmaVersion: "latest",
-        sourceType: "module",
-    },
-
-    settings: {
-        react: {
-            version: "detect",
+        settings: {
+            react: {
+                version: 'detect',
+            },
         },
     },
-}, {
-    files: ["**/*.js", "**/*.jsx"],
-}]);
+])
